f87f993c602aad018fd735da74d49ca632e18238
Erick Rivas <erivas@informaticaylogistica.com>
2020-12-02 08:06:35 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<article>
+    <h1>Volveremos pronto!</h1>
+    <div>
+        <p>Disculpe las molestias, pero estamos realizando algunas tareas de mantenimiento en este momento. Si lo necesita, siempre puede contactarnos <a>info@rivasdev.tech</a>, de lo contrario, en breve estaremos en línea.</p>
+        <p>&mdash; El equipo de desarrollo</p>
+    </div>
+</article>
